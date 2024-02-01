2d796e060fa0e49acb4c291e2c38a6ccd69c5b92
kujira12 <ha9botan@gmail.com>
2024-02-01 15:29:07 +0900

    合計金額　ポップアップ改修

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -1,10 +1,9 @@
-const priceElement = document.getElementById("product");
+const priceElement = document.getElementById("product"); // sale.htmlのid = "product"よりデータをとってくる
 const numberElement = document.getElementById("number");
 const product = document.getElementById("product");
 let purchases = [];
 
-// (表示する金額・id・商品名の内容を追加)①追加ボタンをクリックした時に、商品名も表示する。（金額、数量、商品名を表示）
-
+// (表示する金額・id・商品名の内容を追加)①商品データを管理するオブジェクトを作成する
 const products = [
     {
         id: 1,
@@ -28,6 +27,7 @@ const products = [
     }
 ]
 
+// 2. 追加ボタンをクリックした時に、商品データの中から該当商品を特定して扱えるようにする
 function add() {
     const price = parseInt(priceElement.value);
     const number = parseInt(numberElement.value);
@@ -37,11 +37,6 @@ function add() {
         number: number,
         product: selectedProduct,
     };
-
-
-
-    // 以下を変更
-
 
     let newPurchase = true; //--1
 
@@ -72,6 +67,7 @@ function display() {
     let string = "";
     // 合計を算出する処理
     for (let i = 0; i < purchases.length; i++) {
+     // 購入履歴の内容の文字列（「○○円（商品名+価格:product.name）が○（${purchases[i].number}）点、」）を返す関数、display()関数
         string += `${purchases[i].product.name}が${purchases[i].number}点\n`;
     }
     return string;
@@ -86,22 +82,27 @@ function subtotal() {
 }
  return sum;
 }
- 
-
-// add()関数、subtotal()関数と、display()関数の値を使い、「○○が○点、○○が○点、小計○○円」とポップアップする処理を加える
 
 function calc() {
     const sum = subtotal();
     const postage = calcPostageFromPurchase(sum)
-    window.alert(`小計は${sum}円、送料は${postage}円、合計は${sum + postage}円です`);
+    let message = "";
+ 
+    for (let i = 0; i < purchases.length; i++) {
+        message += `${purchases[i].product.name}が${purchases[i].number}点\n`;
+    }
+
+    // 合計金額をクリックした際「○○が○点、○○が○点、小計○○円」とポップアップする処理を加える。
+    window.alert(`${message} 送料は${postage}円です。合計は${sum + postage}円です`);
+
     purchases = [];
     priceElement.value = "";
     numberElement.value = "";
 }
 
 
-// subtotal()関数の値を利用するように書き換える
-function calcPostageFromPurchase(小計) {
+// subtotal()関数の値を利用するように書き換える。
+function calcPostageFromPurchase(sum) {
   // 条件分岐とreturn
 if (sum == 0 || sum >= 3000) {
     return 0 ;
@@ -111,7 +112,3 @@ if (sum == 0 || sum >= 3000) {
     return 250 ;
 }
 }
-
-
-
-
